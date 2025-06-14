d636793f483a574d9c8ec9a44ff53f813059ea64
RavinduMalshika <ravindu.malshika@gmail.com>
2025-06-14 16:17:26 +0530

    Changed reserve dialog paddin at mobile

--- src/components/ReserveDialog.jsx
+++ src/components/ReserveDialog.jsx
@@ -1,7 +1,7 @@
 const ReserveDialog = ({ handleClose }) => {
     return (
         <div className="fixed z-50 top-0 w-screen h-screen bg-charcoal/50" onClick={handleClose}>
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col bg-beige text-charcaol text-center gap-y-3 rounded-2xl px-20 py-10 border border-rose" onClick={(e) => e.stopPropagation()}>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col bg-beige text-charcaol text-center gap-y-3 rounded-2xl px-10 md:px-20 py-10 border border-rose" onClick={(e) => e.stopPropagation()}>
                 <h1 className="font-great-vibes text-3xl md:text-4xl text-rose">Make a Reservation</h1>
                 <h2 className="md:text-xl mb-2">Please contact us to place a reservation</h2>
 
